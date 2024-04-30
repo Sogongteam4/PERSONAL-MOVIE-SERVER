e995890d11495050d3d0c981a6ba4dff01710786
mungsil <thddmd2009@gmail.com>
2024-05-01 02:12:31 +0900

    [chore] #1 프로젝트 세팅

---
+++ src/main/java/kgu/softwareEG/personalMovie/PersonalMovieApplication.java
@@ -0,0 +1,13 @@
+package kgu.softwareEG.personalMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalMovieApplication.class, args);
+	}
+
+}
