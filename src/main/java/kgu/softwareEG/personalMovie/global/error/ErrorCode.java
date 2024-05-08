89b6f1419a22a45d4a6fac9ce255db2a2a91bf60
mungsil <thddmd2009@gmail.com>
2024-05-08 15:28:43 +0900

    [feat] #5 서비스 로직 구현

--- src/main/java/kgu/softwareEG/personalMovie/global/error/ErrorCode.java
+++ src/main/java/kgu/softwareEG/personalMovie/global/error/ErrorCode.java
@@ -39,6 +39,7 @@ public enum ErrorCode implements BaseErrorCode{
      * 404 Not Found
      */
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 회원입니다."),
+    CHOICE_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 선택지입니다."),
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "엔티티를 찾을 수 없습니다."),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND,"요청한 리소스를 찾을 수 없습니다."),
 
